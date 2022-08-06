01cf0fd5102a32801b6b39c748aad6c654b3a6e9
1gnoramus <elenwarface@gmail.com>
2022-08-06 20:31:29 +0600

    'ProfilePage'

--- lib/main_pages/profile_page.dart
+++ lib/main_pages/profile_page.dart
@@ -13,6 +13,13 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final _auth = FirebaseAuth.instance;
 
+  String name = 'ФИО';
+  int age = 21;
+  String sex = 'Мужской';
+  String phoneNumber = '+7-778-057-66-72';
+  String email = 'a@mail.ru';
+  String rank = 'А+';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,11 +43,162 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Column(
-            children: [],
+          Expanded(
+            child: ListView(children: [
+              Container(
+                margin: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              child: Center(
+                                child: Text(
+                                  'ФОТО',
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                ),
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 30.0),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20.0),
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: Text('Редактировать',
+                                  style: TextStyle(
+                                      fontSize: 10.0, color: Colors.black54)),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        Column(
+                          children: [
+                            ProfileMainInfoPiece(
+                              text: name,
+                            ),
+                            ProfileMainInfoPiece(
+                              text: 'Возраст:   ${age.toString()}',
+                            ),
+                            ProfileMainInfoPiece(
+                              text: 'Пол:  ${sex}',
+                            ),
+                            ProfileMainInfoPiece(
+                              text: phoneNumber,
+                            ),
+                            ProfileMainInfoPiece(
+                              text: email,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(20.0),
+                          child: Text(
+                            'A+',
+                            textAlign: TextAlign.center,
+                            style:
+                                TextStyle(fontSize: 40.0, color: Colors.black),
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.greenAccent,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(70.0))),
+                        ),
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        ProfileMainInfoPiece(text: "Ваш рейтинг: $rank")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          child: Center(
+                            child: Icon(
+                              Icons.graphic_eq,
+                              size: 120.0,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            '*посмотрите как составляется график рейтинга',
+                            style:
+                                TextStyle(fontSize: 10.0, color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Icon(
+                              Icons.star_rate,
+                              color: Colors.black,
+                              size: 80,
+                            ),
+                            Text('4.8',
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.black))
+                          ],
+                        ),
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        Text('Список отзывов',
+                            style:
+                                TextStyle(fontSize: 25.0, color: Colors.black))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ]),
           ),
           BottomPanelWidget()
         ],
+      ),
+    );
+  }
+}
+
+class ProfileMainInfoPiece extends StatelessWidget {
+  ProfileMainInfoPiece({required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 14.0, color: Colors.black),
+      ),
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.symmetric(horizontal: 50.0),
+      width: 220.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(5.0),
+        ),
       ),
     );
   }
