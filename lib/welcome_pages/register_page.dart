d7e006e1a0f4421f7a62c2bff3a02c8d4b6107b3
1gnoramus <elenwarface@gmail.com>
2022-09-03 22:32:47 +0600

    'pubspec'

--- lib/welcome_pages/register_page.dart
+++ lib/welcome_pages/register_page.dart
@@ -5,7 +5,6 @@ import '../components/constants.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegisterPage extends StatefulWidget {
   static String id = 'register_screen';
