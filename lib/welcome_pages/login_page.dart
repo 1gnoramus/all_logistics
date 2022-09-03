34cb66c9da90e1fe2578b8630d0aeb27ab3e0c24
1gnoramus <elenwarface@gmail.com>
2022-09-03 22:25:46 +0600

    'Mod'

--- lib/welcome_pages/login_page.dart
+++ lib/welcome_pages/login_page.dart
@@ -4,7 +4,6 @@ import '../components/rounded button.dart';
 import 'package:flutter/material.dart';
 import '../components/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginPage extends StatefulWidget {
   static String id = 'login_screen';
@@ -21,103 +20,99 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: showSpinner,
-      child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Flexible(
-                child: Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/search.png'),
-                  ),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Flexible(
+              child: Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/search.png'),
                 ),
               ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                  style: TextStyle(color: Colors.black38),
-                  keyboardType: TextInputType.emailAddress,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  decoration: kTextFieldInputDecoration.copyWith(
-                      icon: Icon(
-                        Icons.email_rounded,
-                        size: 20.0,
-                        color: Colors.orangeAccent,
-                      ),
-                      hintText: 'Введите почту')),
-              SizedBox(
-                height: 16.0,
-              ),
-              TextField(
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
                 style: TextStyle(color: Colors.black38),
-                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
                 textAlign: TextAlign.center,
                 onChanged: (value) {
-                  password = value;
+                  email = value;
                 },
                 decoration: kTextFieldInputDecoration.copyWith(
                     icon: Icon(
-                      Icons.password,
+                      Icons.email_rounded,
                       size: 20.0,
                       color: Colors.orangeAccent,
                     ),
-                    hintText: 'Введите пароль'),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              RoundedButton(
-                color: Colors.orangeAccent,
-                buttonText: 'Войти',
-                onTap: () async {
+                    hintText: 'Введите почту')),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black38),
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: kTextFieldInputDecoration.copyWith(
+                  icon: Icon(
+                    Icons.password,
+                    size: 20.0,
+                    color: Colors.orangeAccent,
+                  ),
+                  hintText: 'Введите пароль'),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(
+              color: Colors.orangeAccent,
+              buttonText: 'Войти',
+              onTap: () async {
+                setState(() {
+                  showSpinner = true;
+                });
+                try {
+                  final user = await _auth.signInWithEmailAndPassword(
+                      email: email, password: password);
+                  if (user != null) {
+                    Navigator.pushNamed(context, LoadingScreen.id);
+                  } else {}
                   setState(() {
-                    showSpinner = true;
+                    showSpinner = false;
                   });
-                  try {
-                    final user = await _auth.signInWithEmailAndPassword(
-                        email: email, password: password);
-                    if (user != null) {
-                      Navigator.pushNamed(context, LoadingScreen.id);
-                    } else {}
-                    setState(() {
-                      showSpinner = false;
-                    });
-                  } catch (e) {
-                    setState(() {
-                      showSpinner = false;
-                    });
-                    Alert(
-                      context: context,
-                      type: AlertType.error,
-                      title: "Пароль неверный. Ну или почта, хз",
-                      desc: "Вынь свои глаза из жёпы и введи пароль правильно",
-                      buttons: [
-                        DialogButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: Text(
-                            'Попробовать еще раз',
-                            style:
-                                TextStyle(color: Colors.black38, fontSize: 20),
-                          ),
+                } catch (e) {
+                  setState(() {
+                    showSpinner = false;
+                  });
+                  Alert(
+                    context: context,
+                    type: AlertType.error,
+                    title: "Неверно введены данные Вашего аккаунта",
+                    desc: "Проверьте заполненные данные и попробуйте еще раз",
+                    buttons: [
+                      DialogButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text(
+                          'Повторить',
+                          style: TextStyle(color: Colors.black38, fontSize: 20),
                         ),
-                      ],
-                    ).show();
-                  }
-                },
-              ),
-            ],
-          ),
+                      ),
+                    ],
+                  ).show();
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
--- lib/welcome_pages/register_page.dart
+++ lib/welcome_pages/register_page.dart
@@ -1,8 +1,9 @@
-import 'package:all_log/main_pages/order_page.dart';
 import 'package:all_log/welcome_pages/loading_screen.dart';
 import '../components/rounded button.dart';
 import 'package:flutter/material.dart';
 import '../components/constants.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
@@ -18,89 +19,102 @@ class _RegisterPageState extends State<RegisterPage> {
   bool showSpinner = false;
   String email = '';
   String password = '';
+  Color passwordTextColor = Colors.yellow;
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: showSpinner,
-      child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Flexible(
-                child: Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/search.png'),
-                  ),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Flexible(
+              child: Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/search.png'),
                 ),
               ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                  style: TextStyle(color: Colors.black38),
-                  keyboardType: TextInputType.emailAddress,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  decoration: kTextFieldInputDecoration.copyWith(
-                      icon: Icon(
-                        Icons.email_rounded,
-                        size: 20.0,
-                        color: Colors.orangeAccent,
-                      ),
-                      hintText: 'Введите почту')),
-              SizedBox(
-                height: 16.0,
-              ),
-              TextField(
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
                 style: TextStyle(color: Colors.black38),
-                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
                 textAlign: TextAlign.center,
                 onChanged: (value) {
-                  password = value;
+                  email = value;
                 },
                 decoration: kTextFieldInputDecoration.copyWith(
                     icon: Icon(
-                      Icons.password,
+                      Icons.email_rounded,
                       size: 20.0,
                       color: Colors.orangeAccent,
                     ),
-                    hintText: 'Введите пароль'),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              RoundedButton(
-                color: Colors.deepOrange,
-                buttonText: 'Зарегистрироваться',
-                onTap: () async {
-                  setState(() {
-                    showSpinner = true;
-                  });
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                    if (newUser != null) {
-                      Navigator.pushNamed(context, LoadingScreen.id);
-                    }
+                    hintText: 'Введите почту')),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black38),
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: kTextFieldInputDecoration.copyWith(
+                  icon: Icon(
+                    Icons.password,
+                    size: 20.0,
+                    color: Colors.orangeAccent,
+                  ),
+                  hintText: 'Введите пароль'),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(
+              color: Colors.deepOrange,
+              buttonText: 'Зарегистрироваться',
+              onTap: () async {
+                setState(() {
+                  showSpinner = true;
+                });
 
-                    setState(() {
-                      showSpinner = false;
-                    });
-                  } catch (e) {
-                    print(e);
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email, password: password);
+                  if (newUser != null) {
+                    Navigator.pushNamed(context, LoadingScreen.id);
                   }
-                },
-              ),
-            ],
-          ),
+
+                  setState(() {
+                    showSpinner = false;
+                  });
+                } catch (e) {
+                  Alert(
+                    context: context,
+                    type: AlertType.error,
+                    title: "Пароль должен содержатьт минимум 6 символов",
+                    desc: "Придумайте новый пароль",
+                    buttons: [
+                      DialogButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text(
+                          'Повторить',
+                          style: TextStyle(color: Colors.black38, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ).show();
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
